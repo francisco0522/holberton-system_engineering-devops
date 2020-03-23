d78d5a9f9833d36304751c6b0f5b58c94ac9f48c
francisco0522 <pacho0522@gmail.com>
2020-03-23 17:48:26 -0500

    Adding task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Using Puppet, create a manifest that kills.
 exec { 'killmenow':
-    command  => 'pkill -f killmenow',
-    path     => '/usr/bin',
+    command => 'pkill -f killmenow',
+    path    => '/usr/bin',
 }
